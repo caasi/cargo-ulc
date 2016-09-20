a390884dbe7f7ef3bcd6f936ca38e0715435f711
caasi Huang <caasi.igd@gmail.com>
2016-09-20 23:06:41 +0800

    Create my first enum struct!

---
+++ README.md
@@ -0,0 +1,5 @@
+# ulc
+
+  Try to follow [this tutorial][lambda] to implement my lambda calculus interpreter in Rust.
+
+  [lambda]: http://tadeuzagallo.com/blog/writing-a-lambda-calculus-interpreter-in-javascript/
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::fmt;
+
+// recursive enum should have its lifetime
+enum Term<'a> {
+    App(&'a Term<'a>, &'a Term<'a>),
+    Lam(String, &'a Term<'a>),
+    Var(String),
+}
+
+impl<'a> fmt::Display for Term<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            &Term::App(ref a, ref b) => write!(f, "({})({})", a, b),
+            &Term::Lam(ref s, ref a) => write!(f, "\\{} {}", s, a),
+            &Term::Var(ref s) => write!(f, "{}", s),
+        }
+    }
+}
+
 fn main() {
-    println!("hw");
+    let var1 = &Term::Var("a".to_string());
+    let lam1 = &Term::Lam("b".to_string(), var1);
+    let lam2 = &Term::Lam("a".to_string(), lam1);
+    let var2 = &Term::Var("x".to_string());
+    let app = &Term::App(lam2, var2);
+    println!("{}", app);
 }
